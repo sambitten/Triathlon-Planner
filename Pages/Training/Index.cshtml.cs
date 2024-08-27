923328ba4b0f5eed1db6e65b7a5e2d98845c6c41
Sam Bitten <sambitten@hotmail.co.uk>
2024-08-27 15:09:08 +0100

    Completed sorting and filtering for tracked training
    Can now filter by exercise and sort by date, type of exercise, distance, and time taken

--- Pages/Training/Index.cshtml.cs
+++ Pages/Training/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using EventTrainer.Data;
 using EventTrainer.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EventTrainer.Pages.Training
 {
@@ -21,9 +22,63 @@ namespace EventTrainer.Pages.Training
 
         public IList<TrainingDone> TrainingDone { get;set; } = default!;
 
+
+        [BindProperty(SupportsGet = true)]
+        public TrainingDone.TypeOfExercise SelectedExercise { get; set; }
+
+        public SelectList Exercises { get; set; }
+
+
+        [BindProperty(SupportsGet = true)]
+        public string SortField { get; set; } = "Date";
+
+
         public async Task OnGetAsync()
         {
-            TrainingDone = await _context.TrainingDone.ToListAsync();
+            var Trainings = from c in _context.TrainingDone
+                            select c;
+
+            switch (SortField)
+            {
+                case "Date":
+                    Trainings = Trainings.OrderBy(c => c.Date);
+                    break;
+                case "Exercise type":
+                    Trainings = Trainings.OrderBy(c => c.ExerciseType);
+                    break;
+                case "Distance":
+                    Trainings = Trainings.OrderBy(c => c.Distance);
+                    break;
+                case "Time":
+                    Trainings = Trainings.OrderBy(c => c.TimeTaken);
+                    break;
+            }
+
+            if (!SelectedExercise.Equals(null))
+            {
+                if (SelectedExercise == Models.TrainingDone.TypeOfExercise.Cycle)
+                {
+                    Trainings = Trainings.Where(c => c.ExerciseType == Models.TrainingDone.TypeOfExercise.Cycle);
+                }
+                else if (SelectedExercise == Models.TrainingDone.TypeOfExercise.Run)
+                {
+                    Trainings = Trainings.Where(c => c.ExerciseType == Models.TrainingDone.TypeOfExercise.Run);
+                }
+                else if (SelectedExercise == Models.TrainingDone.TypeOfExercise.Swim)
+                {
+                    Trainings = Trainings.Where(c => c.ExerciseType == Models.TrainingDone.TypeOfExercise.Swim);
+                }
+            }
+
+            List<string> exerciseNames = Enum.GetValues(typeof(TrainingDone.TypeOfExercise))
+                                         .Cast<TrainingDone.TypeOfExercise>()
+                                         .Select(e => e.ToString())
+                                         .ToList();
+
+            Exercises = new SelectList(exerciseNames);
+
+            TrainingDone = await Trainings.ToListAsync();
         }
+
     }
 }
